9a2392ced83f1cb0c19533e11d693867810723d3
Selim BAYINDIR <byndr28@gmail.com>
2022-08-12 09:43:00 +0300

    Repository Pattern

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,4 +1,6 @@
 ﻿
+using Core.DataAccess.EntityFramework;
+using Core.DataAccess.EntityFramework.Repository;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework.Context;
 using Entities.Concrete;
@@ -12,58 +14,11 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfRepositoryBase<Product, NortwindContext>, IProductDal
     {
-        public void Add(Product product)
-        {
-            using (NortwindContext context = new NortwindContext())
-            {
-                var addedEntity=context.Entry(product);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();  
-            }
-        }
-
-        public void Delete(Product product)
-        {
-            using (NortwindContext context = new NortwindContext())
-            {
-                var deletedEntity = context.Entry(product);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NortwindContext context = new NortwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);        
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NortwindContext context = new NortwindContext())
-            {
-               return filter==null
-                    ?context.Set<Product>().ToList()
-                    :context.Set<Product>().Where(filter).ToList();
-            }
-        }
         public List<Product> MyList()
         {
             throw new NotImplementedException();
-        }
-
-        public void Update(Product product)
-        {
-            using (NortwindContext context = new NortwindContext())
-            {
-                var updatedEntity = context.Entry(product);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
         }
     }
 }
